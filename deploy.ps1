83f47650c3c70d0d7860b3037cc9b274f9888b88
Tierre Neal <tneal@hitachisolutions.com>
2022-12-20 22:13:07 -0600

    added compute

--- deploy.ps1
+++ deploy.ps1
@@ -1,2 +1,23 @@
-## Remove --what-if to deploy
+<# 
+Powershell Helpfuls
+
+
+Get-AzVMSize -Location westus2
+
+ Get-AzVMImagePublisher -location westus2 | where {$_.PublisherName.Contains("Microsoft")}
+ Get-AzVMImageOffer -Location westus2 -PublisherName 'MicrosoftWindowsServer'
+ Get-AzVMImagesku -Location westus2 -PublisherName 'MicrosoftWindowsServer'  -Offer WindowsServer
+ Get-AzVMImage -Location westus2 -PublisherName 'MicrosoftWindowsServer'  -Offer WindowsServer -Skus 2022-datacenter-core-g2
+
+Popular Publisher Names
+
+
+ Canonical
+ MicrosoftWindowsServer
+ MicrosoftWindowsDesktop
+ RedHat
+
+
+Remove --what-if to deploy 
+#>
 az deployment sub create --name TestDeploy --location eastus --template-file 'main.bicep' --parameters 'parameters.json' --what-if
