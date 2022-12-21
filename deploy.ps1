ac50460fe5417e4b94280c0517da10b7263ba6d3
Tierre Neal <tneal@hitachisolutions.com>
2022-12-20 20:16:46 -0600

    updated deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,1 +1,2 @@
-New-AzSubscriptionDeployment -name Demosubdeploy -Location eastus -TemplateFile "./main.bicep"  -WhatIf
+## Remove --what-if to deploy
+az deployment sub create --name TestDeploy --location eastus --template-file 'main.bicep' --parameters 'parameters.json' --what-if
